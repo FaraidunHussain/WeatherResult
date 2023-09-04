07855c86f91bb1da953e386960c049491407e6e2
Faraidun Hussain <133172907+FaraidunHussain@users.noreply.github.com>
2023-09-05 02:20:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const API_KEY = `f670bb5175b0cf10a292dd9f230b8d9a`
+const form = document.querySelector("form");
+const search = document.querySelector("#search");
+const weather = document.querySelector("#weather");
+const getWeather = async(city, country) => {weather.innerHTML = `<h2> Loading... <h2>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json()
+    return showWeather(data)
+}
+
+const showWeather = (data) => {weather.innerHTML = `
+        <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        </div>
+        <div>
+            <h2>${data.main.temp} ℃</h2>
+            <h4> ${data.weather[0].main} </h4>
+        </div>
+    `
+}
+
+form.addEventListener(
+    "submit",
+    function(event) {
+        getWeather(search.value)
+        event.preventDefault();
+    }
+)
